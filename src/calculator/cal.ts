3e47f23f6955bf581e19ade1e85d5b624fc9bb7f
kundan sharma <sharmakundan227@gmail.com>
2024-10-06 16:40:04 +0530

    Green phase: return passing in handle negative numbers by throwing an error

--- src/calculator/cal.ts
+++ src/calculator/cal.ts
@@ -11,6 +11,11 @@ class Calculator {
     }
 
     const numbers = input.split(delimiter).map((num) => parseInt(num, 10));
+    const negatives = numbers.filter((num) => num < 0);
+    if (negatives.length > 0) {
+      throw new Error(`negative numbers not allowed: ${negatives.join(",")}`);
+    }
+
     return numbers.reduce((sum, num) => sum + num, 0);
   }
 }
