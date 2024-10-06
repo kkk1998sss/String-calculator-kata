41253a7d1f248fa023113096302ab00bb3b24f69
kundan sharma <sharmakundan227@gmail.com>
2024-10-06 16:34:23 +0530

    Green phase: return Handle newlines between numbers

--- src/calculator/cal.ts
+++ src/calculator/cal.ts
@@ -3,7 +3,7 @@ class Calculator {
     if (input === "") {
       return 0;
     }
-    const numbers = input.split(',').map((num) => parseInt(num, 10));
+    const numbers = input.split(/,|\n/).map((num) => parseInt(num, 10));
     return numbers.reduce((sum, num) => sum + num, 0);
   }
 }
