2f1f1434d1eba87e99dbdc37943a544ca770388c
kundan sharma <sharmakundan227@gmail.com>
2024-10-06 16:14:06 +0530

    Green phase: return the same number when only one number is provided

--- src/calculator/cal.ts
+++ src/calculator/cal.ts
@@ -1,7 +1,10 @@
 class Calculator {
-    static add(input: string): number {
+  static add(input: string): number {
+    if (input === "") {
       return 0;
     }
+    return parseInt(input, 10);
   }
-  
-  export default Calculator;
+}
+
+export default Calculator;
