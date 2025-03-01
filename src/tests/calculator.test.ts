cac3ec89feca9e22d96a97bc094fe3a313cf978e
Kundan <sharmakundan227@gmail.com>
2025-03-01 22:58:36 +0530

    Green phase: return the same number when only one number is provided

--- src/tests/calculator.test.ts
+++ src/tests/calculator.test.ts
@@ -3,5 +3,8 @@ describe('Calculator', () => {
   test('should return 0 for an empty string', () => {
     expect(Calculator.add("")).toBe(0);
   });
+  test('should return the same number when only one number is provided', () => {
+    expect(Calculator.add("1")).toBe(1);
+  });
   
 });
